238ea9a815e7ce01827703d53771e78652e22160
Peaceteddie <Peaceteddie@gmail.com>
2021-08-17 14:04:18 +0200

    Initial commit

---
+++ Assets/Annotation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Annotation : MonoBehaviour
+{
+    Camera Main;
+    CanvasGroup Group;
+    Transform Child;
+    void Start()
+    {
+        Main = Camera.main;
+        Child = transform.GetChild(0);
+        Group = GetComponent<CanvasGroup>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Child.LookAt(Main.transform);
+
+        Group.alpha = Mathf.Clamp01(Vector3.Dot(transform.forward, Main.transform.forward) - .5f) * 2f;
+    }
+}
---
+++ Assets/CameraControls.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControls : MonoBehaviour
+{
+    int index;
+
+    [SerializeField] float Speed;
+    [SerializeField] float Distance;
+
+    [SerializeField] Transform Target;
+    [SerializeField] Transform[] Hotspots;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+            index = index < Hotspots.Length - 1 ? ++index : 0;
+
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+            index = index > 0 ? --index : Hotspots.Length - 1;
+
+        UpdateTransform();
+    }
+
+    void UpdateTransform()
+    {
+        var adjustedPosition = new Vector3(Target.position.x, Hotspots[index].position.y, Target.position.z);
+
+        var direction = Hotspots[index].position - adjustedPosition;
+
+        var newPosition = adjustedPosition + direction.normalized * Distance;
+
+        transform.position = Vector3.MoveTowards(transform.position, newPosition, Speed * Time.deltaTime);
+
+        transform.LookAt(Hotspots[index].position);
+    }
+}
